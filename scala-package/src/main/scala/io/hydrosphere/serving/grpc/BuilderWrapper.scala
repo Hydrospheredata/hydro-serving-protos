ab805f7f854f4ccd4b134184808ffe7e9d3ea8fa
Zajs <aga.zajs@gmail.com>
2018-03-05 09:22:31 -0800

    added intercept to BuilderWrapper

--- scala-package/src/main/scala/io/hydrosphere/serving/grpc/BuilderWrapper.scala
+++ scala-package/src/main/scala/io/hydrosphere/serving/grpc/BuilderWrapper.scala
@@ -3,11 +3,15 @@ package io.hydrosphere.serving.grpc
 import io.grpc._
 
 case class BuilderWrapper[T <: ServerBuilder[T]](builder: ServerBuilder[T]) {
-    def addService(service: ServerServiceDefinition): BuilderWrapper[T] = {
-      BuilderWrapper(builder.addService(service))
-    }
-
-    def build: Server = {
-      builder.build()
-    }
+  def addService(service: ServerServiceDefinition): BuilderWrapper[T] = {
+    BuilderWrapper(builder.addService(service))
   }
+
+  def intercept(service: ServerInterceptor): BuilderWrapper[T] = {
+    BuilderWrapper(builder.intercept(service))
+  }
+
+  def build: Server = {
+    builder.build()
+  }
+}
