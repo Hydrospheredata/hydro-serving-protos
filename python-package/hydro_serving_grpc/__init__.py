2a22fa1250454c17373c6f819a8f1923b24a2642
Bulat <lb6557@gmail.com>
2018-03-28 12:16:43 +0300

    Scala and python fixes

--- python-package/hydro_serving_grpc/__init__.py
+++ python-package/hydro_serving_grpc/__init__.py
@@ -2,10 +2,8 @@ from hydro_serving_grpc.contract.model_contract_pb2 import *
 from hydro_serving_grpc.contract.model_field_pb2 import *
 from hydro_serving_grpc.contract.model_signature_pb2 import *
 
-from hydro_serving_grpc.tf.tensor_info_pb2 import *
 from hydro_serving_grpc.tf.tensor_pb2 import *
 from hydro_serving_grpc.tf.tensor_shape_pb2 import *
-from hydro_serving_grpc.tf.tensor_slice_pb2 import *
 from hydro_serving_grpc.tf.types_pb2 import *
 
 from hydro_serving_grpc.tf.api.model_pb2 import *
@@ -15,5 +13,4 @@ from hydro_serving_grpc.tf.api.prediction_service_pb2_grpc import *
 
 from hydro_serving_grpc.kafka.kafka_messages_pb2 import *
 
-
-
+from hydro_serving_grpc.manager.applications_pb2 import *
