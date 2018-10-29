fa55a1f47989fef7708383334516795d2ea21255
Bulat <lb6557@gmail.com>
2018-10-29 14:06:26 +0300

    Fix UINT dtype fields

--- scala-package/src/main/scala/io/hydrosphere/serving/tensorflow/tensor/UintTensor.scala
+++
@@ -1,9 +0,0 @@
-package io.hydrosphere.serving.tensorflow.tensor
-
-object UintTensor {
-  def protoLens[T <: IntTensor[_]] = new TensorProtoLens[T] {
-    override def getter: TensorProto => Seq[Int] = _.uint32Val
-
-    override def setter: (TensorProto, Seq[Int]) => TensorProto = _.withUint32Val(_)
-  }
-}
