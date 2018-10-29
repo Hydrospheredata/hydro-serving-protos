56456e97ec1acce205c57d276125368be812d888
Bulat <lb6557@gmail.com>
2018-10-29 18:53:50 +0300

    Fix monitoring import

---
+++ python-package/hydro_serving_grpc/monitoring/__init__.py
@@ -0,0 +1,2 @@
+from hydro_serving_grpc.monitoring.data_profile_types_pb2 import *
+from hydro_serving_grpc.monitoring.monitoring_pb2 import *
--- python-package/tests/test_import.py
+++ python-package/tests/test_import.py
@@ -9,3 +9,7 @@ class ImportTest(unittest.TestCase):
     def test_onnx_import_check(self):
         a = hsg.onnx.TensorProto()
         print(type(a))
+
+    def test_monitoring_import_check(self):
+        from hydro_serving_grpc.monitoring import DataProfileType
+        print(DataProfileType.keys())
