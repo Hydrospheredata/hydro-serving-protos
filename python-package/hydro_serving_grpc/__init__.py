e2662ae67fac9c5186a07e1b206cdb6b8e2b3c63
Bulat <lb6557@gmail.com>
2019-03-26 12:38:48 +0300

    Add trace_data to PredictResponse Add status method to PredictService

--- python-package/hydro_serving_grpc/__init__.py
+++ python-package/hydro_serving_grpc/__init__.py
@@ -1,20 +1,11 @@
-from hydro_serving_grpc.contract.model_contract_pb2 import *
-from hydro_serving_grpc.contract.model_field_pb2 import *
-from hydro_serving_grpc.contract.model_signature_pb2 import *
-from hydro_serving_grpc.contract.data_profile_types_pb2 import *
-
-from hydro_serving_grpc.tf.tensor_pb2 import *
-from hydro_serving_grpc.tf.tensor_shape_pb2 import *
-from hydro_serving_grpc.tf.types_pb2 import *
-
-from hydro_serving_grpc.tf.api.model_pb2 import *
-from hydro_serving_grpc.tf.api.predict_pb2 import *
-from hydro_serving_grpc.tf.api.prediction_service_pb2 import *
-from hydro_serving_grpc.tf.api.prediction_service_pb2_grpc import *
-
-from hydro_serving_grpc.kafka.kafka_messages_pb2 import *
-
-from hydro_serving_grpc.manager.applications_pb2 import *
-
+import hydro_serving_grpc.tf
 import hydro_serving_grpc.onnx
 import hydro_serving_grpc.monitoring
+import hydro_serving_grpc.manager
+import hydro_serving_grpc.kafka
+import hydro_serving_grpc.contract
+
+from hydro_serving_grpc.tf.tensor_pb2 import TensorProto
+from hydro_serving_grpc.tf.tensor_shape_pb2 import TensorShapeProto
+from hydro_serving_grpc.tf.types_pb2 import *
+from hydro_serving_grpc.tf.api import *
---
+++ python-package/hydro_serving_grpc/tf/__init__.py
@@ -0,0 +1,3 @@
+from hydro_serving_grpc.tf.tensor_pb2 import *
+from hydro_serving_grpc.tf.tensor_shape_pb2 import *
+from hydro_serving_grpc.tf.types_pb2 import *
