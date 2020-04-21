c96d0399fb114e41cc87db33018b80b243ee3b9a
Bulat Lutfullin <blutfullin@hydrosphere.io>
2020-04-21 20:55:46 +0300

    Remove python release for now. Add signingKey param

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,24 +2,24 @@ def repository = 'hydro-serving-protos'
 
 
 def buildAndPublishReleaseFunction={
-    configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: 'python-package/.pypirc', variable: 'PYPI_SETTINGS')]) {
-        sh """#!/bin/bash
-            python3 -m venv venv
-            source venv/bin/activate
-            pip install wheel~=0.34.2
-            pip install twine 
-            pip install -r ${env.WORKSPACE}/python-package/requirements.txt &&
-            make PYTHON=python3 all &&
-            make PYTHON=python3 test &&
-            python -m twine upload --config-file ${env.WORKSPACE}/python-package/.pypirc -r pypi ${env.WORKSPACE}/python-package/dist/*
-        """
-    }
+//    configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: 'python-package/.pypirc', variable: 'PYPI_SETTINGS')]) {
+//        sh """#!/bin/bash
+//            python3 -m venv venv
+//            source venv/bin/activate
+//            pip install wheel~=0.34.2
+//            pip install twine
+//            pip install -r ${env.WORKSPACE}/python-package/requirements.txt &&
+//            make PYTHON=python3 all &&
+//            make PYTHON=python3 test &&
+//            python -m twine upload --config-file ${env.WORKSPACE}/python-package/.pypirc -r pypi ${env.WORKSPACE}/python-package/dist/*
+//        """
+//    }
 
     def curVersion = getVersion()
     withCredentials([file(credentialsId: 'SonatypeSigningKey', variable: 'SONATYPE_KEY_PATH')]) {
         sh "gpg --import ${SONATYPE_KEY_PATH}"
         dir("${env.WORKSPACE}/scala-package") {
-            sh "sbt -DappVersion=${curVersion} 'set pgpPassphrase := Some(Array())' +publishSigned"
+            sh "sbt -DappVersion=${curVersion} 'set pgpPassphrase := Some(Array())' 'set pgpSigningKey:= Some(\"${SONATYPE_KEY_PATH}\")'  +publishSigned"
             sh "sbt -DappVersion=${curVersion} 'sonatypeReleaseAll'"
         }
     }
