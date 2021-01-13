572f52404f6c6cb4ae5281902228b01080bf84a8
akastav <switch@bk.ru>
2021-01-13 15:00:22 +0300

    add alloy-empy if nothing change

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -206,7 +206,7 @@ def releaseService(String xVersion, String yVersion){
   withCredentials([usernamePassword(credentialsId: 'HydroRobot_AccessToken', passwordVariable: 'password', usernameVariable: 'username')]) {
       //Set global git
       sh script: "git diff", label: "show diff"
-      sh script: "git commit -a -m 'Bump to $yVersion'", label: "commit to git"
+      sh script: "git commit --allow-empty -a -m 'Bump to $yVersion'", label: "commit to git"
       sh script: "git push https://$username:$password@github.com/Hydrospheredata/${SERVICENAME}.git --set-upstream master", label: "push all file to git"
       sh script: "git tag -a $yVersion -m 'Bump $xVersion to $yVersion version'",label: "set git tag"
       sh script: "git push --set-upstream origin master --tags",label: "push tag and create release"
